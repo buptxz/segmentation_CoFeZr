2eb3fe86752452e7f419dc28d223fd165b5bdc90
fang-ren <ren.fang.ren@gmail.com>
2017-02-08 17:22:41 -0800

    n.a.

--- silicon_bckgrd_fit.py
+++ silicon_bckgrd_fit.py
@@ -12,7 +12,7 @@ import numpy as np
 path = 'C:\\Research_FangRen\\Data\\Metallic_glasses_data\\CoZrFe_ternary\\1D\\raw_1D'
 
 silicon_file = os.path.join(path, 'Sample1_24x24_t30_0233_1D.csv')
-spectrum_file = os.path.join(path, 'Sample1_24x24_t30_0001_1D.csv')
+spectrum_file = os.path.join(path, 'Sample1_24x24_t30_0002_1D.csv')
 
 silicon = np.genfromtxt(silicon_file, delimiter=',')
 spectrum = np.genfromtxt(spectrum_file, delimiter= ',')
@@ -33,7 +33,7 @@ plt.plot(Q, intensity)
 #
 
 plt.subplot(212)
-f = 0.98
+f = 1
 plt.plot(Q, intensity - bckgrd*f-160)
 plt.plot(Q, np.zeros(len(Q)))
 
