3581760515248e596cc87027cebbac0f2869f410
fang-ren <ren.fang.ren@gmail.com>
2017-07-25 20:49:01 -0700

    Added 177 pads

--- scripts/CombiView/save_spectra_177.py
+++ scripts/CombiView/save_spectra_177.py
@@ -7,11 +7,11 @@ import numpy as np
 ## user input
 folder_path = '..\\..\\data\\1D\\'
 base_filename1 = 'Sample1_24x24_t30_'
-file_num = 177
-
 basefile_path1 = folder_path + base_filename1
 basefile_paths = [basefile_path1]  # if there are more than one folder you want to organize, put all in this bracket
 
 index = 1
+file_num = 177
+
 data = read_data(file_num, index, basefile_paths, wavelength= 1.54)
 np.savetxt('..//..//data//CombiView//XRD_spectra_CoFeZr.txt', data, delimiter='\t', fmt='%10.5f', comments='')
